ed6498e118532097e3c1ae52cc69f78524286ab0
Louisonn <97410495+Louisonn@users.noreply.github.com>
2022-01-26 01:35:09 +0100

    little changes

--- Software/projet-f103/Debug/appli/PIN.d
+++
@@ -1,5 +0,0 @@
-appli/PIN.o: ../appli/PIN.c ../appli/PIN.h ../lib/bsp/macro_types.h
-
-../appli/PIN.h:
-
-../lib/bsp/macro_types.h:
--- Software/projet-f103/Debug/appli/button.d
+++
@@ -1,6 +0,0 @@
-appli/button.o: ../appli/button.c ../appli/button.h \
- ../lib/bsp/macro_types.h
-
-../appli/button.h:
-
-../lib/bsp/macro_types.h:
